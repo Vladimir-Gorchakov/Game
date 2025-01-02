89e996538f7fd7ddd3afc2d1edbeb0e04b4edd3b
Vladimir-Gorchakov <17vova11@gmail.com>
2025-01-02 12:43:32 +0300

    style fix flake 8

--- client.py
+++ client.py
@@ -51,13 +51,13 @@ class client:
             self.server_socket.sendall(json_data)
         except Exception as e:
             logger.error(f"Error sending JSON data: {e}, {traceback.format_exc()}")
-    
+
     def close(self):
         try:
             self.server_socket.close()
             logger.info("Socket closed successfully")
         except Exception as e:
-             logger.error(f"Error closing socket: {e}, {traceback.format_exc()}")
+            logger.error(f"Error closing socket: {e}, {traceback.format_exc()}")
 
 
 class game:
@@ -69,7 +69,7 @@ class game:
         self.WINDOW_WIDTH = 1200
         self.WINDOW_HEIGHT = 800
         self.PLAYER_COLOR = None  # example: (0, 0, 255)
-        
+
         # get server socket from client class
         self.client = client(SERVER_IP, SERVER_PORT)
         self.server_socket = self.client.server_socket
@@ -91,7 +91,7 @@ class game:
 
         # starting game
         self.game_loop()
-        
+
     def autorization(self):
         # get player name
         self.server_socket.sendall("get_player_name".encode("utf-8"))
@@ -100,10 +100,9 @@ class game:
 
         # get current status of the game
         self.server_socket.sendall("get_status".encode("utf-8"))
-        self.update_status() # excpect {"players" : {player1 : {coords: [x, y], ...}, player2 : {...}}}
+        self.update_status()  # excpect {"players" : {player1 : {coords: [x, y], ...}, player2 : {...}}}
         assert isinstance(self.status, dict)
         assert self.status
-
 
     def game_loop(self):
         while self.running:
@@ -128,7 +127,7 @@ class game:
                 self.running = False
 
             # If button is presed check what are pressed
-            if event.type == pygame.KEYDOWN: 
+            if event.type == pygame.KEYDOWN:
                 match event.key:
                     case pygame.K_w:
                         self.player_info["coords"][1] += 10
