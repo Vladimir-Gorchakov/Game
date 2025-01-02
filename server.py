d081863cda0e16e223e405d99ce70cd91d2c89e0
Shevtsov Aleksey <shevtsovaa@my.msu.ru>
2025-01-02 12:47:48 +0300

    Stylefix flake 8

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import selectors
 import socket
-import string
 import types
 import json
 
+
 class PlayerStatuses:
     all_players_info = dict()
-    
+
     def __init__(self):
         pass
 
@@ -28,8 +28,6 @@ class PlayerStatuses:
             player_info (dict): {coords: [x, y], ...}
         """
         self.all_players_info[nickname] = player_info
-    
-
 
 
 class Server:
@@ -41,7 +39,7 @@ class Server:
         self.server_ip, self.server_port = server_ip, server_port
         self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.nickname_gen = self.nickname_generator()
-    
+
     def get_connections(self):
         self.server_sock.bind((self.server_ip, self.server_port))
         self.server_sock.listen()
@@ -54,9 +52,9 @@ class Server:
                 events = self.sel.select(timeout=None)
                 for key, mask in events:
                     if key.data is None:
-                        self.accept_connection(key.fileobj)  
+                        self.accept_connection(key.fileobj)
                     else:
-                        self.handle_client(key, mask)  
+                        self.handle_client(key, mask)
         except KeyboardInterrupt:
             print("Server shutting down...")
             self.server_sock.close()
@@ -67,7 +65,7 @@ class Server:
     def accept_connection(self, sock: socket.socket):
         conn, addr = sock.accept()
         print(f"Connected by {addr}")
-        conn.setblocking(False)  
+        conn.setblocking(False)
         data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
         self.sel.register(conn, selectors.EVENT_READ | selectors.EVENT_WRITE, data=data)
         self.connected_users[conn.getpeername()] = next(self.nickname_gen)
@@ -75,25 +73,25 @@ class Server:
     def handle_client(self, key: selectors.SelectorKey, mask: int):
         sock: socket = key.fileobj
         data: types.SimpleNamespace = key.data
-        if mask & selectors.EVENT_READ:  
-            recv_data = sock.recv(1024)  
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)
             if recv_data:
                 print(f"Received {recv_data} from {data.addr}")
                 self.parse_recv(sock, recv_data, data)
             else:
                 print(f"Closing connection to {data.addr}")
-                self.sel.unregister(sock)  
+                self.sel.unregister(sock)
                 sock.close()
-        if mask & selectors.EVENT_WRITE and data.outb:  
+        if mask & selectors.EVENT_WRITE and data.outb:
             print(f"Sending {data.outb} to {data.addr}")
             sent = sock.send(data.outb)
             data.outb = data.outb[sent:]
-    
+
     def parse_recv(self, conn: socket.socket, recv_data: bytes, data: types.SimpleNamespace):
         nickname = self.connected_users[conn.getpeername()]
         data_str = recv_data.decode("utf-8")
         match data_str:
-            # TODO при первом подключении должно быть get_player_name 
+            # TODO при первом подключении должно быть get_player_name
             case "get_player_name":
                 data.outb = nickname.encode("utf-8")
                 return
@@ -106,24 +104,24 @@ class Server:
                 data_dict = json.loads(data_str)
                 self.statuses.update_status(nickname=nickname, player_info=data_dict)
                 data.outb = self.encode_dict(self.statuses.getStatuses())
-               
+
     def nickname_generator(self):
         counter = 0
         while True:
             counter += 1
             yield f'player{counter}'
-            
+
     def encode_dict(self, data: dict) -> bytes:
         return json.dumps(data).encode("utf-8")
 
     def decode_dict(self, data: bytes) -> dict:
         return json.loads(data.decode("utf-8"))
-    
-            
+
 
 def main():
     server = Server(server_ip="127.0.0.1", server_port=65432)
     server.get_connections()
+
 
 if __name__ == '__main__':
     main()
